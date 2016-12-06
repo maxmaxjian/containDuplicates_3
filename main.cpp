9cf03a4323a81167d8544d15954b70749ae298af
Jian Wei <jianwei@Jians-MacBook-Pro.local>
2016-12-05 23:18:57 -0500

    second version

--- main.cpp
+++ main.cpp
@@ -1,48 +1,4 @@
-// // solution 1
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// #include <iterator>
-// #include <cmath>
-
-// class solution {
-//   public:
-//     bool containDuplicates(const std::vector<int> & nums, int k, int t) {
-//         size_t start = 0;
-//         while (start <= nums.size()-k) {
-//             // attempt 1
-//             // std::vector<int> temp;
-//             // std::transform(nums.begin()+start, nums.begin()+start+k, std::inserter(temp, temp.begin()),
-//             //           [&](int i){
-//             //               return i - nums[0];
-//             //           });
-//             // if (std::any_of(temp.begin(), temp.end(), [&](int i){return abs(i) > t;}))
-//             //     return true;
-//             // else
-//             //     start++;
-
-//             // attempt 2
-//             // if (start+k < nums.size()) {
-//             //     auto it_pair = std::minmax_element(nums.begin()+start, nums.begin()+start+k+1);
-//             //     std::cout << " start = " << start << ", min = " << *it_pair.first << ", max = " << *it_pair.second << std::endl;
-//             //     if (*it_pair.second-*it_pair.first <= t)
-//             //         return true;
-//             //     start++;
-//             // }
-//             // else {
-//             //     auto it_pair = std::minmax_element(nums.begin()+start, nums.end());
-//             //     std::cout << " start = " << start << ", min = " << *it_pair.first << ", max = " << *it_pair.second << std::endl;
-//             //     if (*it_pair.second-*it_pair.first <= t)
-//             //         return true;
-//             //     start++;
-//             // }
-            
-//         }
-//         return false;
-//     }
-// };
-
-// // solution 2 brute force
+// // solution 1 brute force
 // #include <iostream>
 // #include <vector>
 // #include <algorithm>
@@ -53,8 +9,10 @@
 //     bool containNearbyAlmostDuplicates(const std::vector<int> & nums, int k, int t) {
 //         int begin, end;
 //         for (int center = 0; center < nums.size(); ++center) {
-//             begin = (center > k-1) ? center-k : 0;
-//             end = (center+k < nums.size()) ? center+k : nums.size()-1;
+//             // begin = (center > k-1) ? center-k : 0;
+//             // end = (center+k < nums.size()) ? center+k : nums.size()-1;
+// 	    begin = center;
+// 	    end = center+k;
 //             std::cout << "center  = " << center << " -> From " << begin << " to " << end << std::endl;
 //             int max = *std::max_element(nums.begin()+begin, nums.begin()+end+1,
 //                                         [&](int i, int j){
@@ -69,7 +27,7 @@
 //     }
 // };
 
-// solution
+// solution 2
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -78,51 +36,19 @@
 class solution {
   public:
     bool containNearbyAlmostDuplicates(const std::vector<int> & nums, int k, int t) {
-        std::map<int, std::queue<int>> maxmap, minmap;
-        int curr = 0, begin, end;
-        while (curr < nums.size()) {
-            begin = curr > k ? curr-k : 0;
-            end = curr+k < nums.size() ? curr+k : nums.size()-1;
-            if (maxmap.empty()) {
-                auto it = std::minmax_element(nums.begin()+begin, nums.begin()+end+1);
-                for (int i = begin; i <= end; i++) {
-                    if (nums[i] == *it.first) {
-                        if (minmap.find(curr) == minmap.end())
-                            minmap[curr] = std::queue<int>();
-                        minmap[curr].push(i);
-                    }
-                    else if (nums[i] == *it.second) {
-                        if (maxmap.find(curr) == maxmap.end())
-                            maxmap[curr] = std::queue<int>();
-                        maxmap[curr].push(i);
-                    }
-                }
-            }
-            else {
-                if (nums[end] < nums[minmap[curr-1].front()]) {
-                    minmap[curr] = std::queue<int>{end};
-                }
-                else {
-                    minmap[curr] = minmap[curr-1];
-                    while (!minmap[curr].empty() && minmap[curr].front() < begin)
-                        minmap[curr].pop();
-                    if (minmap[curr].empty())
-                        minmap[curr].push();
-                    else if (*it.first == nums[minmap[curr].front()])
-                        minmap[curr].push(it.first-nums.begin());
-                }
-
-                if (*it.second > nums[maxmap[curr-1].front()]) {
-                    maxmap[curr] = std::queue<int>{it.second-nums.begin()};
-                }
-                else {
-                    maxmap[curr] = maxmap[curr-1];
-                    while (!maxmap[curr].empty() && maxmap[curr].front() < begin)
-                        maxmap[curr].pop();
-                    if ()
-                }
-            }
-        }
+	std::map<int,int> dict;
+	int i, j;
+	for (i = 0, j = i; i < nums.size(); i++) {
+	    while (j-i <= k) {
+		dict[nums[j]] = j;
+		j++;
+	    }
+	    auto it = dict.lower_bound(nums[i]-t);
+	    if (it != dict.end() && abs(it->first-nums[i]) <= t)
+		return true;
+	    dict.erase(nums[i]);
+	}
+	return false;
     }
 };
 
@@ -131,9 +57,13 @@ int main() {
     // int k = 2, t = 4;      // expected result = true
     
     std::vector<int> nums{7,1,3};
-    int k = 2, t = 3;         // expected result = false
+    int k = 2, t = 3;         // expected result = true
     
     solution soln;
     bool exist = soln.containNearbyAlmostDuplicates(nums, k, t);
     std::cout << std::boolalpha << exist << std::endl;
+    // if (exist)
+    // 	std::cout << "There is a center within the distance of " << k << " the difference is not great than " << t << std::endl;
+    // else
+    // 	std::cout << "There isn't a center within the distance of " << k << " the difference is not great than " << t << std::endl;
 }
